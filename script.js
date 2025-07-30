b501e79b80c5be98320d143b6f5bd9e97a287f53
unknown <mavello006@gmail.com>
2025-07-30 07:33:37 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+// const images = [
+//     'images/menpic1.jpg',
+//     'images/menpic2.jpg',
+//     'images/menpic3.jpg'
+// ];
+// let index = 0;
+// const hero = document.getElementById('hero');
+
+// function changeHeroBg() {
+//     hero.style.backgroundImage = `url(${images[index]})`;
+//     index = (index + 1) % images.length;
+// }
+// setInterval(changeHeroBg, 3000); // Change every 3 seconds
+// window.onload = changeHeroBg;
